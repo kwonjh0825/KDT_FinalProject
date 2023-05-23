ab2039775008e8c92660240796d51cc0ace6aa04
kwonjh0825 <ghr1700@naver.com>
2023-05-23 16:27:08 +0900

    KP-2 add find_id, delete, logout

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
     path("login/", views.login, name="login"),
+    path('find_id/', views.find_id.as_view(), name='find_id'),
+    path('delete/', views.delete, name='delete'),
+    path('logout/', views.logout, name='logout'),
 ]
--- app_accounts/views.py
+++ app_accounts/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
+from .models import User
 # Create your views here.
 
 def login(request):
     return render(request, 'accounts/login.html')
+
+
+class find_id(View):
+    def get(self, request):
+        return render(request, 'accounts/find_id.html')
+
+    def post(self, request):
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+        try:
+            # 입력받은 정보로 유저 검색
+            user_found = User.objects.get(email=email, first_name=first_name, last_name=last_name)
+            context = {
+                'user_found': user_found,
+            }
+            # 일치하는 정보의 유저가 있다면 결과 페이지로 이동
+            return render(request, 'accounts/find_id_result.html', context)
+        
+        except:
+            # 일치하는 정보의 유저가 없으면 메시지 출력 후 다시 아이디를 찾을 수 있도록 함
+            context = {
+                'message': '일치하는 정보가 없습니다. '
+            }
+            return render(request, 'accounts/find_id.html', context)
+
+@login_required
+class delete(View):
+    def post(self, request):
+        # 계정 정보 삭제 후 로그아웃
+        request.user.delete()
+        auth_logout(request)
+        return redirect('planet:main')
+    
+@login_required
+class logout(View):
+    def post(self, request):
+        auth_logout(request)
+        return redirect('planet:main')
