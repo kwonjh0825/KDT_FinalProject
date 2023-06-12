3ad02175e96e858422f58f1b22d8a7de9f37584d
sotthang <sotthang@gmail.com>
2023-06-12 11:46:35 +0900

    update planet create

--- app_planets/models.py
+++ app_planets/models.py
@@ -47,7 +47,7 @@ class Planet(models.Model):
     is_public_category = (('Private', 'Private'), ('Public', 'Public'))
     is_public          = models.CharField(max_length=10, choices=is_public_category, default='Private')
     # 가입 요청 확인 필요
-    need_confirm_category = ((True, 'Yes'), (False, 'No'))
+    need_confirm_category = ((True, 'Approval'), (False, 'Direct'))
     need_confirm          = models.BooleanField(choices=need_confirm_category, default=False)
     maximum_capacity      = models.DecimalField(default=50, max_digits=1000, decimal_places=0)
     created_by            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
