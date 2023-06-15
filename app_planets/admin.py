160b63f6a96424f36a06b6b8b29009e220255f7e
sotthang <sotthang@gmail.com>
2023-06-15 18:02:06 +0900

    update admin

--- app_planets/admin.py
+++ app_planets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Planet, TermsOfService, Post, Comment, InappropriateWord, Report
+from .models import Planet, TermsOfService, Post, Comment, InappropriateWord, Report, VoteTopic, Vote
 
 # Register your models here.
 
@@ -9,3 +9,5 @@ admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(InappropriateWord)
 admin.site.register(Report)
+admin.site.register(VoteTopic)
+admin.site.register(Vote)
